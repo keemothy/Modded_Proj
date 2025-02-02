0f7fa2663df7b1bdfc6b69e3666c49a175b0a95e
Kim Chu <kimchu@berkeley.edu>
2025-02-01 16:09:50 -0800

    upload test

---
+++ src/main/java/proj/space/mod/SpaceModClient.java
@@ -0,0 +1,10 @@
+package proj.space.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpaceModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
